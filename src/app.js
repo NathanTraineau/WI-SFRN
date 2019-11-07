a4923b6b715484293ba488018058fcda092aa56b
Traineau <nathan.trn@orange.fr>
2019-11-07 10:30:01 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-
+var assert = require('assert');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 const express = require("express");
 let app = new express();
 const verifier = require('alexa-verifier-middleware');
 
+var isFisrtTime = true;
 const alexaRouter = express.Router();
 app.use('/alexa', alexaRouter);
 
@@ -21,32 +22,33 @@ alexaRouter.use(verifier);
 //And request the good url to get the course from the year
 //We should then parse the response to get what the user wants and send a proper json file in response to alexa.
 
-
-alexaRouter.get("/tomorrow", function(req, res) {
+alexaRouter.post("/tomorrow", function(req, res) {
 
     if (req.body.request.type === 'LaunchRequest') {
       res.json(getTomorrowSchedule("STE4"));
+      isFisrtTime = false
     } else if (req.body.request.type === 'IntentRequest') {
       switch (req.body.request.intent.name) {
         case 'GetTomorrowSchedule':
-            res.json(getTomorrowSchedule("IG5"));
+            res.json(getTomorrowSchedule("IG5","1"));
           break;
         default:
             const response = response_to_Alexa("no data")
             res.json(response)
-  
       }
     }
 });
 
+
 app.get("/tomorrow", function(req, res) {
-  if (req.query.class != null){
-    const user_class = req.query.class;
-    res.json(getTomorrowSchedule(user_class)) 
-  }else{
-    const response = response_to_Alexa("no data")
-    return response;
-}
+    if (req.query.class != null){
+      const user_class = req.query.class;
+      res.json(getTomorrowSchedule(user_class,"1"));
+    }
+    else{
+      const response = response_to_Alexa("no data")
+      res.json(response);
+  }
 });
 
 let port = 5000;
@@ -55,18 +57,95 @@ app.listen(port, function() {
 });
 
 
-function getTomorrowSchedule(user_class){
+function requestVerifier(req, res, next) {
+  alexaVerifier(
+    req.headers.signaturecertchainurl,
+    req.headers.signature,
+    req.rawBody,
+    function verificationCallback(err) {
+      if (err) {
+        res.status(401).json({
+          message: 'Verification Failure',
+          error: err
+        });
+      } else {
+        next();
+      }
+    }
+  );
+}
+
+
+getTomorrowSchedule = function(user_class,group){
+    return getDaySchedule(user_class,group,1);
+} 
+
+
+function getDaySchedule(user_class,group, numberOfDayFromToday){
+  //This function take the class user, its group and the number of day from today
+  //With this information it get the right schedule, parse the result and build a response for alexa.
   var d = new Date();
   var tomorrow = new Date();
-  tomorrow.setDate(d.getDate() + 8);
-      const sched = schedule(`https://wave-it.fr/application/cache/json/${user_class}.json`);
-      const parsed = parser(sched,tomorrow.getDate(),tomorrow.getMonth(),tomorrow.getFullYear())
-      const ress = plain_text_array(parsed)
-      const plain_text = ress.join(" ")
-      const response = response_to_Alexa(plain_text)
-      return response;
-  
+  tomorrow.setDate(d.getDate() + numberOfDayFromToday);
+  tomorrow.setMonth(d.getMonth() + numberOfDayFromToday);
+  const sched = schedule(`https://wave-it.fr/application/cache/json/${user_class}.json`);
+  const tomorrowSchedule = scheduleOftheDay(sched,tomorrow.getDate(),tomorrow.getMonth(),tomorrow.getFullYear())
+  //We get the courses of the groupe of our user
+  const tomorrowScheduleParsedGroup = parseGroup(tomorrowSchedule,group)
+  //We sort the course so we have them in the time order
+  const tomorrowScheduleSorted = sortedSchedule(tomorrowScheduleParsedGroup)
+  //We build the sentences to give to alexa per course
+  const tomorrowScheduleToArray = plain_text_array(tomorrowScheduleSorted)
+  //We join it in one string
+  const tomorrowScheduleToPlainText = tomorrowScheduleToArray.join(" ")
+  const response = response_to_Alexa(tomorrowScheduleToPlainText)
+  return response;
 }
+
+parseGroup = function(schedule,group){
+  return schedule.filter(function(course) {
+    if(course.hasOwnProperty("grp")){
+      //The only case we don't return is when there is a group number
+      //and it is not the one given.
+      //We keep every other courses.
+      return course.grp === group.toString() || course.grp === ""
+    }else{
+      return true
+    }
+    
+  })
+}
+
+sortedSchedule = function(scheduleToSort,scheduleSorted = JSON.parse('{"courses":[]}'))
+{
+  //This function get a JSON 
+  //return the object corresponding to the given JSON text
+  //assert(typeof(scheduleToSort) == String.type)
+  if(( scheduleToSort.length === 0 )){
+    //We check whether we finished to sort or not
+    return scheduleSorted
+  }
+
+  var min = 100
+  var mincourse;
+  scheduleToSort.filter( function(course) {
+    
+    var splited = course.start_time.split(":")
+    var hour = splited[0]
+    if(parseInt(hour) < min){
+      mincourse = course
+      min = hour
+    }
+})
+
+var d = scheduleToSort.filter(course => {
+  return course.start_time != mincourse.start_time || course.location != mincourse.location})
+  
+scheduleSorted['courses'].push(mincourse)
+return sortedSchedule(d,scheduleSorted);
+}
+
+
 
 schedule = function(theUrl)
 {
@@ -76,11 +155,10 @@ schedule = function(theUrl)
     return xmlHttp.responseText;
 }
 
-parser = function(sched,day_i,month_i,year_i) {
+    scheduleOftheDay = function(sched,day_i,month_i,year_i) {
     //This function get all of the course from one specific day 
+    //return the object corresponding to the given JSON text
     var s = JSON.parse(sched) 
-    //return s.items.filter(d => d.date == day)
-    
     return s.items.filter(({
       date,
       month,
@@ -89,20 +167,18 @@ parser = function(sched,day_i,month_i,year_i) {
       return date.toLowerCase() === day_i.toString() &&
         month.toLowerCase() === month_i.toString() &&
         year.toLowerCase() === year_i.toString()
-  
     })
-  
   }
 
   plain_text_array = function(parsed){
-      //This function get the info the user should get on the courses
-      const res = parsed.map( function(course) {
+      //This function take a json text as parameter and the info the user should get on the courses
+      console.log("jfkmdfjmsqdfjmqsdl" + parsed.type)
+      const res = parsed.courses.map( function(course) {
           let info = "You have " + course.name + " from " + course.start_time + " to " + course.end_time + " at the location " + course.location;
           return info;
       })
       return res
   }
-
 
   response_to_Alexa = function(plain_text){
     const speechOutput = "<speak>" + plain_text + "</speak>"
@@ -110,11 +186,10 @@ parser = function(sched,day_i,month_i,year_i) {
         "version": "1.0",
         "response": {
           "outputSpeech": {
-            "type": "PlainText",
-            "text": `${plain_text}`     
+            "type": "SSML",
+            "ssml": speechOutput    
           },
           "shouldEndSession": true
         }
       }
-
   }
