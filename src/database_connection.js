a33432cd599ea54cb4dfd7d6262de84f7b61c6f0
Traineau <nathan.trn@orange.fr>
2019-11-11 12:11:21 +0100

    Finish and verify cleaning

--- src/database_connection.js
+++ src/database_connection.js
@@ -8,38 +8,38 @@ const connectionString = 'postgres://yieqxnmnufyqqg:5e7e8ad3d79c26d0011140594608
 
 const client = new Client({
   connectionString: connectionString,
+  ssl: true
 })
 client.connect(function(err){
-  if(err) throw err
-  else console.log("fdsiuqljhgl")
+  //if(err) throw err
+  //else console.log("fdsiuqljhgl")
 })
 
 module.exports = { 
-    // ADD A USER TO THE DATABASE
-    addUser: function(user_id, user_class, user_group, successCallback, errorCallback){
+  //ADD USER TO THE DATABASE
+  addUser: function(user_id,user_class,user_group, successCallback, errorCallback){
     pool.query('insert into "Users" values ($1, $2, $3)',[user_class, user_group,user_id],(err,results) => {
       if (err) errorCallback(err)
       successCallback(results.rows[0])
     })
-    },
+  },
 
-    // UPDATE A USER FROM THE DATABASE
-    updateUser: function(user_id,user_class, user_group, successCallback, errorCallback){
-        pool.query('update "Users" set "class"=$1 , "group"=$2 where "user_id"=$3',[user_class, user_group,user_id],(err,results) => {
-          if (err) errorCallback(err)
-          successCallback(results)
-        })
-    },
+  //UPDATE USER4S INFORMATIONS TO THE DATABASE
+  updateUser: function(user_id,user_class,user_group, successCallback, errorCallback){
+    pool.query('update "Users" set "user_class"=$1 , "user_group"=$2 where "user_id"=$3',[user_class, user_group,user_id],(err,results) => {
+      if (err) errorCallback(err)
+      successCallback(results)
+    })
+  },
 
-    // GET ALL USERS FROM THE DATABASE
-    getUsers: function(successCallback, errorCallback){
-        pool.query(('SELECT * FROM "Users"'),(err, results) => {
-          if (err) errorCallback(err)
-          successCallback(results.rows[0])
-        })
-    },
+  //GET A USER FROM THE DATABASE
+  getUsers: function(successCallback, errorCallback){
+    pool.query(('SELECT * FROM "Users"'),(err, results) => {
+      if (err) errorCallback(err)
+      successCallback(results.rows[0])
+    })
+  },
 
-    // GET A USER WITH SPECIFIC ID FROM THE DATABASE
     getUserById: function(user_id, successCallback, errorCallback){
       const query = {
         text: 'SELECT * FROM "Users" where user_id = $1 ',
